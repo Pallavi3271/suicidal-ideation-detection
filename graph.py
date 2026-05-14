2fc34fb362540e754fdad40f5f853d253754ae00
Pallavi Uppara <214g1a3271@srit.ac.in>
2026-05-14 15:57:15 +0530

    final commit

--- graph.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-data = pd.read_csv('uploadedfile/causes.csv')
-sns.countplot(x='Type',data=data)
-plt.xticks(rotation=90)
-plt.show()
